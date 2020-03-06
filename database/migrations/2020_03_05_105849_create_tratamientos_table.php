940078b6bbb652e9245ec580f1ae2337c58b1dd2
AntonioHern <antoniohern@gmail.com>
2020-03-06 12:48:36 +0100

    Creacion de mensajes

---
+++ database/migrations/2020_03_05_105849_create_tratamientos_table.php
@@ -0,0 +1,39 @@
+
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTratamientosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tratamientos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('paciente');
+            $table->foreign('paciente')->references('dni')->on('pacientes')->onUpdate('cascade');
+            $table->string('nMedicina');
+            $table->float('dosis');
+            $table->float('cada');
+            $table->float('stock');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tratamientos');
+    }
+}
